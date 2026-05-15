d0e20a911d1b6b36ba01295175bb804a61b87fb0
Valentine <38808380+Jayyaah@users.noreply.github.com>
2026-05-15 12:01:00 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,21 @@ function addCard() {
     const cardName = document.getElementById('name').value;
     const cardCategory = document.getElementById('category').value;
     const cardNote = document.getElementById('note').value;
+
+    if (cardName === '' && cardNote === '') {
+        alert('Veuillez remplir les champs "Nom de la carte" et "Note".');
+        return;
+    }
+
     const cardList = document.createElement('li');
         cardList.innerHTML = `
         <h3>${cardName}</h3>
         <p>${cardCategory}</p>
-        <p>${cardNote}</p>
+        <p>Note : ${cardNote}/10</p>
         <button type="button" onclick="this.parentElement.remove()">Supprimer</button>
     `;
     document.getElementById('list').appendChild(cardList);
+
+    document.getElementById('name').value = '';
+    document.getElementById('note').value = '';
 }
